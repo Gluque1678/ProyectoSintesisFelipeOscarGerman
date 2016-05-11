b47eb7f099b7b8d1bc2373fba581a8c06361ed65
OscarOrtiz <93180.joan23@fje.edu>
2016-05-11 11:01:54 +0200

    a21 con insert

--- A21.php
+++ A21.php
@@ -2,14 +2,13 @@
 
 session_start();
 $dni=$_SESSION['dni_datos'];
+// var_dump($dni);
 $tip=$_SESSION['TIP'];
+// var_dump($tip);
 include("conexion.proc.php");
 
-$hora= date ("h:i:s");
-$fecha= date ("d/m/Y");
 
 ?>
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -22,138 +21,166 @@ $fecha= date ("d/m/Y");
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-
-<script type="text/javascript" src="a21.js"></script>
+<script src="js/prueba.js"></script>
+<!--<script type="text/javascript" src="view.js"></script>
+<script type="text/javascript" src="validaFormulario.js"></script>-->
 
 </head>
 <body id="main_body" >
 
-
-
+	
+	
 
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<img id="logo" src="img/logoUrbanaActas.png" alt="">
-				<h4>GUARDIA URBANA</h4>
+				
+				<img id="logo" src="img/Ajun.jpg" width="60px" height="50px" alt="">
+				<h4>Ayuntamiento de L’Hospitalet
+							Area de Seguredad,</br>
+							Convivencia y Civismo
+							Servicio de la Guardia Urbana
+				</h4>
+	<form class="form-incline" class="A21"  method="" action="a21.proc.php" >
+						
 			</div>
+				
+				<div class="panel-body">
+					
+
 
 			
-			<form class="form-incline" class="A21"  method="" action="get">
+					<div class="form-group">
 
 
+				
 
+					</div>	
+			</div>
+
+		
+			
 		<div class="panel panel-success">
 			<div class="panel-heading">
 
 				<div id="datos_acta">
-
+				
 				<h3>Datos del acta</h3>
-
+				
 			</div>
 					<div class="row">
 						<div class="col-xs-2"
+				
 							<label class="sr-only" for="NºDiligencias"></label>
-							<input type="diligencias" name= "diligencias" class="form-control" placeholder="Número de Diligencias"/></br>
+							<input type="diligencias" name= "Numdiligencias" id="Numdiligencias" class="form-control" placeholder="Número de Diligencias"/></br>
+						
+						
 						</div>
-						<div class="col-xs-2">
-							<input type="tip" name= "agentes" class="form-control" placeholder="Agentes" value="<?php echo $tip; ?>" readonly/>
-
-						</div>
-
-						<div class="col-xs-2">
-							<input type="text" name= "dia" class="form-control" placeholder="Fecha" value="<?php echo $fecha; ?>" readonly/>
-						</div>
+					
+						<div class="col-xs-2">		
+						<input type="tip" name= "agentes" id="agentes" class="form-control" placeholder="Agentes" value="<?php echo $tip; ?>" readonly/>
+					</div>
 
 						<div class="col-xs-2">
-							<input type="text" name= "hora" class="form-control" placeholder="hora" value="<?php echo $hora; ?>" readonly/>
+						<input type="date" name= "dia" id="dia" class="form-control" placeholder="Fecha" value=""/>
+						</div>
+
+						<div class="col-xs-1">
+						<input type="time" name= "hora" id="hora" class="form-control" placeholder="hora" value=""/>
 						</div>
 
 						<div class="col-xs-3">
-							<input type="lugar" name= "lugar" class="form-control" placeholder="Lugar" value=""/>
+						<input type="lugar" name= "lugar" id="lugar" class="form-control" placeholder="Lugar" value=""/>
 						</div>
 
+						
 
 				</div>
 		</div>
 			</br></br>
 
 		<div class="panel panel-danger">
-			<div class="panel-heading">
+			<div class="panel-heading">	
 			<h3 style="margin-top: -5px;"> Acta de simptomatologia: Datos de la persona requerida </h3>
+			
+				
+		
 
 				<div class="row">
 					<div class="col-xs-2"
-
+				
 						<label class="sr-only" for="apellidos"></label>
-
-						<input type="apellidos" name= "apellidos" class="form-control" placeholder="Apellidos" value="<?php echo $dni['Primerapellido']; ?>" readonly/>
-
-
+						<input type="apellidos" id="apellidos" name= "apellidos" class="form-control" placeholder="Apellidos" value="<?php echo $dni['Primerapellido']; ?>" readonly/>
+						
+							
+					</div>
+					
+					<div class="col-xs-2">		
+					<input type="nombre" name= "nombre" id="nombre" class="form-control" placeholder="Nombre" value="<?php echo $dni['Nombre']; ?>" readonly/>
 					</div>
 
 					<div class="col-xs-2">
-					<input type="nombre" name= "nombre" class="form-control" placeholder="Nombre" value="<?php echo $dni['Nombre']; ?>" readonly/>
-
-					</div>
-
-					<div class="col-xs-2">
-					<input type="permiso" name= "permiso" class="form-control" placeholder="Núm permiso de conducir" value="<?php echo $dni['Num']; ?>" readonly/>
+					<input type="permiso" name= "permiso" id="permiso" class="form-control" placeholder="Núm permiso de conducir" value="<?php echo $dni['Num']; ?>" readonly/>
 					</div>
 
 					<div class="radio">
-
+			
 				<label>
 					<h4>Sexo &nbsp</h4>
 				</label>
 
 				<label>
-
-					<input type="radio" name="opciones" id="opciones_1"value="opcion_1" checked>
+					
+					<input type="radio" name="sexo" id="sexo" value="Hombre" checked>
 					Hombre &nbsp
 				</label>
 
 				<label>
-
-					<input type="radio" name="opciones" id="opciones_2"value="opcion_2" checked>
+					
+					<input type="radio" name="sexo" id="sexo" value="Mujer" checked>
 					Mujer
 				</label>
 
 				<div class="col-xs-1">
-					<input type="altura" name= "altura" class="form-control" placeholder="Altura" value=""/>
+					<input type="altura" name= "altura" id="altura" class="form-control" placeholder="Altura" value=""/>
 				</div>
 
 				<div class="col-xs-1">
-					<input type="peso" name= "peso" class="form-control" placeholder="Peso" value=""/>
+					<input type="peso" name= "peso" id="peso" class="form-control" placeholder="Peso" value=""/>
 				</div>
-
+				
 			</div>
 
 
 				</div>
 
-
+			
 		</div>
-	</div>
-
+	</div>	
+		
 
 		<div class="panel panel-info">
 			<div class="panel-heading">
 				<div class="Descripcion_conduccion">
-					<label class="Descripcion" for=""><h3 style="margin-top: -5px;">Descripcion de la conduccion</h3></label>
-					<textarea class="form-control" rows="5"></textarea>
-				</div></br>
+					<label class="Descripcion"  for=""><h3 style="margin-top: -5px;">Descripcion de la conduccion</h3></label>
+					<textarea class="form-control" rows="5" id="descripcion" name= "descripcion">
+					</textarea>	
+				</div>
+
+					</br>
+
+
 		</div>
 			</div>
 
-
+			
 		<div class="panel panel-info">
 			<div class="panel-heading">
-
+			
 				<div class="simptoma">
-
+				
 					<label for="simptoma"><h3 style="margin-top: -5px;">Simptomatologia: Se hace constar que la persona requerida presentava la simptomatologia siguiente</h3>
 					</label>
-
+				
 				</div>
 
 			</div>
@@ -163,17 +190,17 @@ $fecha= date ("d/m/Y");
 		<div class="panel panel-success">
 			<div class="panel-heading">
 				<div class="radio">
-
+			
 				<label for="olor"><h4 style="margin-top: -5px;">Habilitosi de alcohol</h4>
-
+					
 				</label>
 
 
 			</div>
 		</div>
-				<label>
-
-					<input type="radio" name="opciones" id="opciones_1"value="opcion_1" checked>
+				<label class="checkbox-inline">
+					
+					<input type="checkbox" name="halitosis" id="halitosis" value="1" checked>
 					El olor que desprende es claramente detectable
 				</label>
 
@@ -181,61 +208,78 @@ $fecha= date ("d/m/Y");
 			</div>
 
 		<div class="panel panel-success">
-			<div class="panel-heading">
+			<div class="panel-heading">	
 
 				<div class="radio">
-
+			
 					<label for="fisica"><h4 style="margin-top: -5px;">Constitución física&nbsp</h4>
-
+					
 					</label>
+				
+
 			</div>
-		</div>
+		</div>		
 
 				<label>
-					<input type="radio" name="opciones" id="opciones_1"value="opcion_1" checked>Corpulenta</label>
+					
+					<input type="radio" name="constitucion" id="constitucion" value="Corpulenta" checked>
+					Corpulenta
+				</label>
+
 				<label>
-					<input type="radio" name="opciones" id="opciones_1"value="opcion_1" checked>Mediana</label>
+					
+					<input type="radio" name="constitucion" id="constitucion" value="Mediana" checked>
+					Mediana
+				</label>
+				
 				<label>
-					<input type="radio" name="opciones" id="opciones_1"value="opcion_1" checked>Pequeña</label>
+					
+					<input type="radio" name="constitucion" id="constitucion" value="Pequeña" checked>
+					Pequeña
+				</label>
+
 				<hr />
+				
 				</div>
 
 		<div class="panel panel-success">
 			<div class="panel-heading">
 
 				<div class="Comportamiento">
-
-					<label for="fisica"><h4 style="margin-top: -5px;">Comportamiento&nbsp</h4></label>
+			
+					<label for="fisica"><h4 style="margin-top: -5px;">Comportamiento&nbsp</h4>
+					
+					</label>
 					</br>
 			</div>
 		</div>
-
+			
 			<label class="checkbox-inline">
-  				<input type="checkbox" id="checkboxEnLinea1" value="opcion_1"> Agresivo
+  				<input type="checkbox" id="Comportamiento_agresivo" name="Comportamiento_agresivo" value="1"> Agresivo
+			</label>
+			
+			<label class="checkbox-inline">
+ 				 <input type="checkbox" id="Comportamiento_insultante" name="Comportamiento_insultante" value="1"> Insultante
+			</label>
+			
+			<label class="checkbox-inline">
+ 			 <input type="checkbox" id="Comportamiento_irrespetuoso" name="Comportamiento_irrespetuoso" value="1"> Irrespetuoso
 			</label>
 
 			<label class="checkbox-inline">
- 				 <input type="checkbox" id="checkboxEnLinea2" value="opcion_2"> Insultante
+ 			 <input type="checkbox" id="Comportamiento_euforico" name="Comportamiento_euforico" value="1"> Eufórico
 			</label>
 
 			<label class="checkbox-inline">
- 				 <input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Irrespetuoso
+ 			 <input type="checkbox" id="Comportamiento_muyloquaz" name="Comportamiento_muyloquaz" value="1"> Locuaz
 			</label>
 
 			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Eufórico
+ 			 <input type="checkbox" id="Comportamiento_excitado" name="Comportamiento_excitado" value="1"> Excitado
 			</label>
 
 			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Locuaz
-			</label>
-
-			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Excitado
-			</label>
-
-			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Variaciones obtadas de comportamiento o estado de ánimo
+ 			 <input type="checkbox" id="Comportamiento_variaciones" name="checkboxEnLinea" value="1"> Variaciones obtadas de comportamiento o estado de ánimo
 			</label>
 		</div>
 
@@ -245,41 +289,53 @@ $fecha= date ("d/m/Y");
 			<div class="panel-heading">
 
 				<div class="Descripcion_Comportamiento">
-					<label class="comportamiento" for=""><h4 style="margin-top: -5px;">Descripción del comportamiento</h4></label>
-					<textarea class="form-control" rows="5"></textarea>
-				</div></br>
+					<label class="comportamiento" for=""><h4 style="margin-top: -5px;">Descripción del comportamiento</h4>
+					</label>
+					<textarea class="form-control" rows="5" name="descripcioncomportamiento" id="descripcioncomportamiento">
+					</textarea>	
+				</div>
+
+				</br>
+
+
 			</div>
 		</div>
 
 	<div class="panel panel-success">
 			<div class="panel-heading">
+
 				<div class="Ojos">
-					<label for="ojos"><h4 style="margin-top: -5px;">Ojos-Mirada&nbsp</h4>
+			
+				<label for="ojos"><h4 style="margin-top: -5px;">Ojos-Mirada&nbsp</h4>
+					
 				</label>
 				</br>
 			</div>
 	</div>
 
 			<label class="checkbox-inline">
-  				<input type="checkbox" id="checkboxEnLinea1" value="opcion_1"> Dilatación pupila
+  				<input type="checkbox" id="Mirada_midriaticos" name="Mirada_midriaticos" value="1"> Dilatación pupila
+			</label>
+			
+			<label class="checkbox-inline">
+ 				 <input type="checkbox" id="Mirada_mioticos" name="Mirada_mioticos" value="1"> Disminución pupila
+			</label>
+			
+			<label class="checkbox-inline">
+ 			 <input type="checkbox" id="Mirada_brillantes" name="Mirada_brillantes" value="1"> Brillantes
 			</label>
 
 			<label class="checkbox-inline">
- 				 <input type="checkbox" id="checkboxEnLinea2" value="opcion_2"> Disminución pupila
+ 			 <input type="checkbox" id="Mirada_somnolienta" name="Mirada_somnolienta" value="1"> Somnolientos
 			</label>
 
 			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea3" value="opcion_3"> Brillantes
+ 			 <input type="checkbox" id="Mirada_perdida" name="Mirada_perdida" value="1"> Perdida
 			</label>
 
-			<label class="checkbox-inline">
- 				<input type="checkbox" id="checkboxEnLinea4" value="opcion_4"> Somnolientos
-			</label>
+			
 
-			<label class="checkbox-inline">
- 			 	<input type="checkbox" id="checkboxEnLinea5" value="opcion_5"> Perdida
-			</label>
-
+			
 		</div>
 
 		</br>
@@ -293,16 +349,18 @@ $fecha= date ("d/m/Y");
 					</label></br>
 				</div>
 			</div>
-
-					<input id="pastosa" name="pastosa" class="pastosa" maxlength="5" value="" type="checkbox">
-					<label for="pastosa">Pastosa</label>
-
-					<input id="clara" name="clara" class="disminucion" maxlength="5" value="" type="checkbox">
+					
+					<input id="Habla_pastosa" name="Habla_pastosa" maxlength="5" value="1" type="checkbox">
+					<label for="pastosa">Pastosa
+					</label>
+				
+					<input id="Habla_clara" name="Habla_clara" maxlength="5" value="1" type="checkbox">
 					<label for="clara">Clara</label>
-
-					<input id="vacilante" name="vacilante" class="vacilante" maxlength="5" value="" type="checkbox">
-					<label for="vacilante">Vacilante</label>
-
+				
+					<input id="Habla_vacilante" name="Habla_vacilante" maxlength="5" value="1" type="checkbox">
+					<label for="vacilante">Vacilante
+					</label>		
+				
 		</div>
 
 
@@ -315,16 +373,16 @@ $fecha= date ("d/m/Y");
 			</div>
 		</div>
 
-				<input id="respuestas" name="respuestas" class="respuestas" maxlength="5" value="" type="checkbox">
+				<input id="Expresion_ininteligibles" name="Expresion_ininteligibles" maxlength="5" value="1" type="checkbox">
 				<label for="respuestas">Respuestas Ininteligibles</label>
-
-				<input id="incoherente" name="incoherente" class="incoherente" maxlength="5" value="" type="checkbox">
+				
+				<input id="Expresion_incoherentes" name="Expresion_incoherentes" maxlength="5" value="1" type="checkbox">
 				<label for="incoherente">Respuestas Incoherentes</label>
 
-				<input id="repetitivo" name="repetitivo" class="otro" maxlength="5" value="" type="checkbox">
+				<input id="Expresion_repetitivo" name="Expresion_repetitivo" maxlength="5" value="1" type="checkbox">
 				<label for="repetitivo">Repetitivo</label>
 
-				<input id="ideas" name="ideas" class="ideas" maxlength="5" value="" type="checkbox">
+				<input id="Expresion_fugaideas" name="Expresion_fugaideas" maxlength="5" value="1" type="checkbox">
 				<label for="Fuga de ideas">Fuga de ideas</label><hr />
 		</div>
 
@@ -337,17 +395,17 @@ $fecha= date ("d/m/Y");
 			</div>
 		</div>
 
-				<input id="" name="vacilante" class="vacilante" maxlength="5" value="" type="checkbox">
+				<input id="Psicomotricidad_vacilante" name="Psicomotricidad_vacilante" maxlength="5" value="1" type="checkbox">
 				<label for="vacilante">Vacilante</label>
+				
+				<input id="Psicomotricidad_imprecision" name="Psicomotricidad_imprecision" maxlength="5" value="1" type="checkbox">
+				<label for="distancias">Imprecision en los movimientos</label>
 
-				<input id="distancias" name="distancias" class="distancias" maxlength="5" value="" type="checkbox">
-				<label for="distancias">Falsa apreciación de las distancias</label>
+				<input id="Psicomotricidad_disminucionreflejos" name="Psicomotricidad_disminucionreflejos" maxlength="5" value="1" type="checkbox">
+				<label for="movimientos">Disminución de los reflejos</label>
 
-				<input id="movimientos" name="movimientos" class="movimientos" maxlength="5" value="" type="checkbox">
-				<label for="movimientos">Movimientos Oscilantes de la Verticalidad</label>
-
-				<input id="reflejos" name="reflejos" class="reflejos" maxlength="5" value="" type="checkbox">
-				<label for="reflejos">Disminución de los reflejos</label>
+				<input id="Psicomotricidad_apreciacionfalsa" name="Psicomotricidad_apreciacionfalsa" maxlength="5" value="1" type="checkbox">
+				<label for="reflejos">Falsa apreciación de las distancias</label>
 
 				<hr />
 
@@ -362,7 +420,9 @@ $fecha= date ("d/m/Y");
 
 			</div>
 		</div>
-				<textarea rows="4" cols="185"></textarea>
+				<textarea rows="4" cols="185" id="Descripcionmotricidad" name="Descripcionmotricidad">
+				</textarea>
+
 	</div>
 
 	<div class="panel panel-danger">
@@ -370,17 +430,16 @@ $fecha= date ("d/m/Y");
 			<div class="firma de los agentes">
 
 			<label for="agentes"><h4>Firma de los agentes</h4></label>
+			<input type="submit"/>
 			</div>
 		</div>
 
 	</div>
 
-	</form>
-		<button type="button" class="btn btn-danger">Registrar</button>
 		<script src="js/jquery.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
 		</script>
-
+	
 		</div>
 	</body>
 
---
+++ a21.proc.php
@@ -0,0 +1,135 @@
+<?php
+	session_start();
+	$dni=$_SESSION['dni_datos'];
+
+
+	include("conexion.proc.php");
+
+if (empty($_REQUEST['halitosis'])) {
+		$_REQUEST['halitosis']=0;
+	}
+if (empty($_REQUEST['Comportamiento_agresivo'])) {
+		$_REQUEST['Comportamiento_agresivo']=0;
+	}
+if (empty($_REQUEST['Comportamiento_insultante'])) {
+		$_REQUEST['Comportamiento_insultante']=0;
+	}
+if (empty($_REQUEST['Comportamiento_irrespetuoso'])) {
+		$_REQUEST['Comportamiento_irrespetuoso']=0;
+	}
+if (empty($_REQUEST['Comportamiento_euforico'])) {
+		$_REQUEST['Comportamiento_euforico']=0;
+	}
+if (empty($_REQUEST['Comportamiento_muyloquaz'])) {
+		$_REQUEST['Comportamiento_muyloquaz']=0;
+	}
+if (empty($_REQUEST['Comportamiento_excitado'])) {
+		$_REQUEST['Comportamiento_excitado']=0;
+	}
+if (empty($_REQUEST['Comportamiento_variaciones'])) {
+		$_REQUEST['Comportamiento_variaciones']=0;
+	}
+if (empty($_REQUEST['Mirada_midriaticos'])) {
+		$_REQUEST['Mirada_midriaticos']=0;
+	}
+if (empty($_REQUEST['Mirada_mioticos'])) {
+		$_REQUEST['Mirada_mioticos']=0;
+	}
+if (empty($_REQUEST['Mirada_brillantes'])) {
+		$_REQUEST['Mirada_brillantes']=0;
+	}
+if (empty($_REQUEST['Mirada_somnolienta'])) {
+		$_REQUEST['Mirada_somnolienta']=0;
+	}
+if (empty($_REQUEST['Mirada_perdida'])) {
+		$_REQUEST['Mirada_perdida']=0;
+	}
+if (empty($_REQUEST['Habla_pastosa'])) {
+		$_REQUEST['Habla_pastosa']=0;
+	}
+if (empty($_REQUEST['Habla_clara'])) {
+		$_REQUEST['Habla_clara']=0;
+	}
+if (empty($_REQUEST['Habla_vacilante'])) {
+		$_REQUEST['Habla_vacilante']=0;
+	}
+if (empty($_REQUEST['Expresion_ininteligibles'])) {
+		$_REQUEST['Expresion_ininteligibles']=0;
+	}
+if (empty($_REQUEST['Expresion_incoherentes'])) {
+		$_REQUEST['Expresion_incoherentes']=0;
+	}
+if (empty($_REQUEST['Expresion_repetitivo'])) {
+		$_REQUEST['Expresion_repetitivo']=0;
+	}
+if (empty($_REQUEST['Expresion_fugaideas'])) {
+		$_REQUEST['Expresion_fugaideas']=0;
+	}
+if (empty($_REQUEST['Psicomotricidad_vacilante'])) {
+		$_REQUEST['Psicomotricidad_vacilante']=0;
+	}
+if (empty($_REQUEST['Psicomotricidad_imprecision'])) {
+		$_REQUEST['Psicomotricidad_imprecision']=0;
+	}
+if (empty($_REQUEST['Psicomotricidad_apreciacionfalsa'])) {
+		$_REQUEST['Psicomotricidad_apreciacionfalsa']=0;
+	}
+if (empty($_REQUEST['Psicomotricidad_disminucionreflejos'])) {
+		$_REQUEST['Psicomotricidad_disminucionreflejos']=0;
+	}
+if (empty($_REQUEST['Psicomotricidad_movimientooscilante'])) {
+		$_REQUEST['Psicomotricidad_movimientooscilante']=0;
+	}
+
+	$sqla21 = "INSERT INTO f_a21pl (`DNI`,`Numdiligencias`, `TIP1`, `Dia`, `Hora`, `Lugar`, `Nombre`, `Primerapellido`, 
+		`Permiso`, `Sexo`, `Altura`, `Peso`, `Descripcionconduccion`, `Halitosi`, `Constitucion`, `Comportamiento_agresivo`, 
+		`Comportamiento_insultante`, `Comportamiento_irrespetuoso`, `Comportamiento_euforico`, `Comportamiento_muyloquaz`, `Comportamiento_excitado`, 
+		`Comportamiento_variaciones`, `Descripcioncomportamiento`, `Mirada_midriaticos`, `Mirada_mioticos`, `Mirada_brillantes`, `Mirada_somnolienta`, 
+		`Mirada_perdida`, `Habla_pastosa`, `Habla_clara`, `Habla_vacilante`, `Expresion_ininteligibles`, `Expresion_incoherentes`, `Expresion_repetitivo`, 
+		`Expresion_fugaideas`, `Psicomotricidad_vacilante`, `Psicomotricidad_imprecision`, `Psicomotricidad_disminucionreflejos`, `Psicomotricidad_apreciacionfalsa`, 
+		`Psicomotricidad_movimientooscilante`, `Descripcionmotricidad`) 
+		VALUES ('$dni[Num]',
+			'$_REQUEST[Numdiligencias]',
+			'$_REQUEST[agentes]',
+			'$_REQUEST[dia]',
+			'$_REQUEST[hora]',
+			'$_REQUEST[lugar]',
+			'$_REQUEST[nombre]',
+			'$_REQUEST[apellidos]',
+			'$_REQUEST[permiso]',
+			'$_REQUEST[sexo]',
+			'$_REQUEST[altura]',
+			'$_REQUEST[peso]',
+			'$_REQUEST[descripcion]',
+			'$_REQUEST[halitosis]',
+			'$_REQUEST[constitucion]',
+			'$_REQUEST[Comportamiento_agresivo]',
+			'$_REQUEST[Comportamiento_insultante]',
+			'$_REQUEST[Comportamiento_irrespetuoso]',
+			'$_REQUEST[Comportamiento_euforico]',
+			'$_REQUEST[Comportamiento_muyloquaz]',
+			'$_REQUEST[Comportamiento_excitado]',
+			'$_REQUEST[Comportamiento_variaciones]',
+			'$_REQUEST[descripcioncomportamiento]',
+			'$_REQUEST[Mirada_midriaticos]',
+			'$_REQUEST[Mirada_mioticos]',
+			'$_REQUEST[Mirada_brillantes]',
+			'$_REQUEST[Mirada_somnolienta]',
+			'$_REQUEST[Mirada_perdida]',
+			'$_REQUEST[Habla_pastosa]',
+			'$_REQUEST[Habla_clara]',
+			'$_REQUEST[Habla_vacilante]',
+			'$_REQUEST[Expresion_ininteligibles]',
+			'$_REQUEST[Expresion_incoherentes]',
+			'$_REQUEST[Expresion_repetitivo]',
+			'$_REQUEST[Expresion_fugaideas]',
+			'$_REQUEST[Psicomotricidad_vacilante]',
+			'$_REQUEST[Psicomotricidad_imprecision]',
+			'$_REQUEST[Psicomotricidad_disminucionreflejos]',
+			'$_REQUEST[Psicomotricidad_apreciacionfalsa]',
+			'$_REQUEST[Psicomotricidad_movimientooscilante]',
+			'$_REQUEST[Descripcionmotricidad]')";
+	$resultado=mysqli_query($congu, $sqla21);
+	echo $sqla21;
+	mysqli_close($congu);
+?>
