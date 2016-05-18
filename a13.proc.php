77b1e893513e72a4b1a8863d02fc86e733d6295e
OscarOrtiz <93180.joan23@fje.edu>
2016-05-18 16:47:44 +0200

    a13 inserts

--- a13.proc.php
+++ a13.proc.php
@@ -6,18 +6,33 @@ $tip=$_SESSION['TIP'];
 include("conexion.proc.php");
 
 
-$sqla13 ="INSERT INTO `f_a13pl`(`DNI`, `Numdiligencias`, `TIP1`, `Lugar`, `Dia`, `Hora`, `Motivoinmobilizacion`, `Numerodenuncia`, `Tipodenuncia`, `Sistemainmobilizacion`, `Quilometrosactuales`, `Vehiculodisposicion`, `Fecharoturainmobilizacion`, `Horaroturainmobilizacion`, `Atestadodesobedienciagrave`, `Desaparicioncausas`, `Otroconductor`, `Ingresodeposito`, `Nombresustituto`, `Permisosustituto`, `Clasesustituto`, `Paisexpedicionsustituto`, `Retiradaingresodeposito`)
-	   VALUES 
+$sqla13 ="INSERT INTO `f_a13pl`(`DNI`, `Numdiligencias`, `TIP1`, `Lugar`, `Dia`, `Hora`, `Motivoinmobilizacion`, `Numerodenuncia`, `Tipodenuncia`, `Sistemainmobilizacion`, `Quilometrosactuales`, `Vehiculodisposicion`, `Fecharoturainmobilizacion`, `Horaroturainmobilizacion`, `Atestadodesobedienciagrave`, `Observaciones`, `Motivolevantamiento`, `Dialevantamiento`, `Horalevantamiento`, `Nombresustituto`, `Permisosustituto`, `Clasesustituto`, `Paisexpedicionsustituto`, `Retiradaingresodeposito`)
+VALUES 
 	   ('$dni[Num]',
 	   	'$_SESSION[diligencias]',
 	   	'$_REQUEST[agentes]',
+	   	'$_REQUEST[lugar]',
+	   	'$_REQUEST[fecha]',
+	   	'$_REQUEST[hora]',
+	   	'$_REQUEST[immobilizacion]',
+	   	'$_REQUEST[denuncia]',
+	   	'$_REQUEST[Tipodenuncia]',
+	   	'$_REQUEST[sistema]',
+	   	'$_REQUEST[kilometros]',
+	   	'$_REQUEST[Vehiculodisposicion]',
+	   	'$_REQUEST[fecharot]',
+	   	'$_REQUEST[horarot]',
+	   	'$_REQUEST[atestadorot]',
+	   	'$_REQUEST[observaciones]',
+	   	'$_REQUEST[Motivolevantamiento]',
+	   	'$_REQUEST[fechalevantamiento]',
+	   	'$_REQUEST[horalevantamiento]',
+	   	'$_REQUEST[nombre]',
+	   	'$_REQUEST[permiso]',
+	   	'$_REQUEST[clase]',
+	   	'$_REQUEST[pais]',
+	   	'$_REQUEST[Retiradaingresodeposito]')";
 
-
-
-
-
-
-	   			)";
 	$resultado=mysqli_query($congu, $sqla13);
 	echo $sqla13;
 	mysqli_close($congu);
