7593b5e8a59589f0a3e02481832e52139e9d52e7
figlesiasma <10000317.joan23@fje.edu>
2016-05-20 11:42:05 +0200

    v2

--- js/script.js
+++ js/script.js
@@ -64,7 +64,41 @@ $(document).ready(function(){
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
+//botón arriba
+$('.ir-arriba').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
 
+});
 
+$(window).scroll(function(){
+  if ( $(this).scrollTop() > 0){
+     $('.ir-arriba').slideDown(300);
+
+  } else {
+    $('.ir-arriba').slideUp(300);
+
+  }
+
+});
+
+$('.ir-arriba').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+
+});
+
+$(window).scroll(function(){
+  if ( $(this).scrollTop() > 0){
+     $('.ir-arriba').slideDown(300);
+
+  } else {
+    $('.ir-arriba').slideUp(300);
+
+  }
+
+});
 
 });
